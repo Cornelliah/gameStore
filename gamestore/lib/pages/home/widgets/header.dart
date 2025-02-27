0afc59adc3d184e5cf2dfee7f4b11fafb13c0f35
Cornelliah <horianehkr@yahoo.com>
2025-02-27 10:07:21 +0100

    home page

---
+++ gamestore/lib/pages/home/widgets/header.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HeaderSection extends StatelessWidget {
+  const HeaderSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+         top: MediaQuery.of(context).padding.top,
+         left: 25,
+         right: 25),
+    
+     child: Row(
+      children: [
+        Column(
+          children: const [
+            Text('Welcome,', style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold), ),
+            SizedBox(height: 5 ,),
+            Text('What would you like to play?', style: TextStyle(color: Colors.white, fontSize: 19, ), )
+
+          ],
+        ), 
+        CircleAvatar(
+          child: Image.asset('assets/images/avatar.png'),
+        ),
+
+      ]),
+    );
+  }
+}
